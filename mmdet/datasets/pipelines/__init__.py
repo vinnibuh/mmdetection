94c402ad271b596d7d52fd4e56672c149919a52d
Yaroslav Ivchenkov <32544750+vinnibuh@users.noreply.github.com>
2020-05-11 21:42:25 +0300

    Update __init__.py

--- mmdet/datasets/pipelines/__init__.py
+++ mmdet/datasets/pipelines/__init__.py
@@ -1,6 +1,7 @@
 from .compose import Compose
 from .formating import (Collect, ImageToTensor, ToDataContainer, ToTensor,
                         Transpose, to_tensor)
+from .formatingDCT import DefaultFormatBundleDCT
 from .instaboost import InstaBoost
 from .loading import (LoadAnnotations, LoadImageFromFile,
                       LoadMultiChannelImageFromFiles, LoadProposals)
@@ -8,11 +9,13 @@ from .test_aug import MultiScaleFlipAug
 from .transforms import (Albu, Expand, MinIoURandomCrop, Normalize, Pad,
                          PhotoMetricDistortion, RandomCrop, RandomFlip, Resize,
                          SegRescale)
+from .transformsDCT import ToDCT, ToDCTUpscaledStatic
 
 __all__ = [
     'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
     'Transpose', 'Collect', 'LoadAnnotations', 'LoadImageFromFile',
     'LoadMultiChannelImageFromFiles', 'LoadProposals', 'MultiScaleFlipAug',
     'Resize', 'RandomFlip', 'Pad', 'RandomCrop', 'Normalize', 'SegRescale',
-    'MinIoURandomCrop', 'Expand', 'PhotoMetricDistortion', 'Albu', 'InstaBoost'
+    'MinIoURandomCrop', 'Expand', 'PhotoMetricDistortion', 'Albu', 'InstaBoost',
+    'ToDCT', 'ToDCTUpscaledStatic', 'DefaultFormatBundleDCT'
 ]
