0ed8d2f96843d010caa56a95eb18e31fb0193ede
Yaroslav Ivchenkov <32544750+vinnibuh@users.noreply.github.com>
2020-05-11 21:46:25 +0300

    Update __init__.py

--- mmdet/models/utils/__init__.py
+++ mmdet/models/utils/__init__.py
@@ -1,3 +1,14 @@
 from .res_layer import ResLayer
+from .conv_module import ConvModule, build_conv_layer
+from .conv_ws import ConvWS2d, conv_ws_2d
+from .norm import build_norm_layer
+from .scale import Scale
+from .weight_init import (bias_init_with_prob, kaiming_init, normal_init,
+                          uniform_init, xavier_init)
 
-__all__ = ['ResLayer']
+__all__ = [
+  'ResLayer',
+  'conv_ws_2d', 'ConvWS2d', 'build_conv_layer', 'ConvModule',
+  'build_norm_layer', 'xavier_init', 'normal_init', 'uniform_init',
+  'kaiming_init', 'bias_init_with_prob', 'Scale'
+]
