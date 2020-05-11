f7a08f624d571da855a42f49c95fd66f5682d64b
Yaroslav Ivchenkov <32544750+vinnibuh@users.noreply.github.com>
2020-05-11 21:44:12 +0300

    Update __init__.py

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -2,5 +2,11 @@ from .hrnet import HRNet
 from .resnet import ResNet, ResNetV1d
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
+from .resnetDCT import ResNetDCT, make_res_layer
+from .resnet_static import ResNetUpscaledStatic
+from .resnet_dynamic import ResNetUpscaledDynamic
+from .resnetDCT_dynamic import ResNetDCT_Dynamic
 
-__all__ = ['ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet']
+__all__ = ['ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet',
+          'ResNetDCT', 'ResNetUpscaledStatic', 'ResNetUpscaledDynamic',
+          'ResNetDCT_Dynamic', 'make_res_layer']
